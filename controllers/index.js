72f2f9c65b4aee9c9e32d8925ae9df63a44910bb
JCCode288 <jendycaprico@gmail.com>
2023-02-09 13:22:29 +0700

    add mock data

--- controllers/index.js
+++ controllers/index.js
@@ -1,15 +1,5 @@
 
 class Controller {
-    static homepage(req, res) {
-        User.findAll({ include: Category })
-            .then(users => res.send(users))
-            .catch(err => res.send(err))
-    }
-    static test(req, res) {
-        User.findAll({ include: UserDetail })
-            .then(users => res.send(users))
-            .catch(err => res.send(err))
-    }
 }
 
 module.exports = Controller
