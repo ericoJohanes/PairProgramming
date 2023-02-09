bfb8871b3251ae57d29ee882b3510405da9a1c58
JCCode288 <jendycaprico@gmail.com>
2023-02-09 09:48:55 +0700

    reset revisi 1, erd fix

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.static('./'))
 
 app.get('/', Controller.homepage)
+app.get('/test', Controller.test)
 
 app.listen(port, (err) => {
     err ? console.log(err) : console.log(`listening http://localhost:${port}`);
--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,13 @@
-const { User, Course, Category } = require('../models')
+const { User, Course, Category, UserDetail } = require('../models')
 
 class Controller {
     static homepage(req, res) {
         User.findAll({ include: Category })
+            .then(users => res.send(users))
+            .catch(err => res.send(err))
+    }
+    static test(req, res) {
+        User.findAll({ include: UserDetail })
             .then(users => res.send(users))
             .catch(err => res.send(err))
     }
